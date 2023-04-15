6722a60fef87041ae3692d0adbe89f624117bf7e
Bryant Nguyen <bryantn9@uci.edu>
2023-04-14 19:30:22 -0700

    setting up file reading/find

--- PartA.py
+++ PartA.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 class Token:
     data = None
@@ -8,3 +9,15 @@ class Token:
 
 if __name__ == "__main__":
     print("pog")
+    
+    filepath = "C:/Users/berya/ICS121/testing.txt"
+    filepath = filepath.split("/")
+
+    target_file = filepath[-1]
+    print(filepath[-1])
+
+    with open(target_file, 'r') as f:
+        text = f.read()
+        text = text.split("\n")
+
+    print(text)
