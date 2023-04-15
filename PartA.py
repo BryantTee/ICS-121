3ae10fd5028d94bf99357f87ab4023d866fb499b
Bryant Nguyen <bryantn9@uci.edu>
2023-04-14 20:18:35 -0700

    word frequencies added

--- PartA.py
+++ PartA.py
@@ -1,10 +1,13 @@
 import os
 import shutil
 
+
+global wordList
+
 class Token:
     data = None
 
-    def __init__(d):
+    def __init__(init, d):
         data = d.lower()
 
 def tokenize(TFP):
@@ -12,10 +15,19 @@ def tokenize(TFP):
         text = f.read()
         text = text.split("\n")
     
-    return text
+    Tokened = [Token(x) for x in text]
+    WordList = Tokened
+    return Tokened
 
-def computeWordFrequencies(word):
-    None
+def computeWordFrequencies(words):
+    retMap = {}
+    for x in words:
+        retMap[x] = 0
+
+    for x in words:
+        retMap[x]+=1
+    
+    return retMap
 
 def print(mapping):
     None 
@@ -31,4 +43,5 @@ if __name__ == "__main__":
     target_file = filepath[-1]
     print(filepath[-1])
 
-    tokenize(target_file)    
+    list = tokenize(target_file)
+    computeWordFrequencies(list)    
