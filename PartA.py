ab1fb13702f16fd3d0e9a75e49d4ca91bca86a6a
Bryant Nguyen <bryantn9@uci.edu>
2023-04-14 19:52:02 -0700

    tokenize setup

--- PartA.py
+++ PartA.py
@@ -7,17 +7,28 @@ class Token:
     def __init__(d):
         data = d.lower()
 
+def tokenize(TFP):
+    with open(TFP, 'r') as f:
+        text = f.read()
+        text = text.split("\n")
+    
+    return text
+
+def computeWordFrequencies(word):
+    None
+
+def print(mapping):
+    None 
+
 if __name__ == "__main__":
     print("pog")
-    
+    print("starting program...")
+
+
     filepath = "C:/Users/berya/ICS121/testing.txt"
     filepath = filepath.split("/")
 
     target_file = filepath[-1]
     print(filepath[-1])
 
-    with open(target_file, 'r') as f:
-        text = f.read()
-        text = text.split("\n")
-
-    print(text)
+    tokenize(target_file)    
